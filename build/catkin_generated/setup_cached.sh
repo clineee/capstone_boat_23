171a5090288df1c971b1e421e7f776eaaff97711
Ethan Cline <clinee@oregonstate.edu>
2023-01-18 14:02:49 -0800

    Inital commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ethan/capstone_boat_23/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/ethan/capstone_boat_23/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ethan/capstone_boat_23/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ethan/capstone_boat_23/src:$ROS_PACKAGE_PATH"
